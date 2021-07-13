16c86a700120a79bf2f3ea20f86306b43a80101a
Jacob Walser <jwalser90@gmail.com>
2021-07-13 02:36:59 -0400

    test-pca9685.py: add argument to set frequency

--- test-pca9685.py
+++ test-pca9685.py
@@ -7,9 +7,11 @@ parser = argparse.ArgumentParser()
 group1 = parser.add_mutually_exclusive_group(required=True)
 group1.add_argument('--get', action='store', nargs='+', type=int)
 group1.add_argument('--set', action='store', nargs='+', type=int)
+group1.add_argument('--frequency', action='store', type=int, default=250)
 args = parser.parse_args()
 
 pca = PCA9685()
+pca.set_pwm_frequency(args.frequency)
 pca.output_enable()
 
 if args.get:
