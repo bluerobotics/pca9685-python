4db37300395d82ac9329def8c4eb46f301dae300
Jacob Walser <jwalser90@gmail.com>
2021-09-29 03:04:53 -0400

    test: wrap main for setup.py console_scripts

--- pca9685/test.py
+++ pca9685/test.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 
-import argparse
-from pca9685 import PCA9685
+def main():
+    import argparse
+    from pca9685 import PCA9685
 
-parser = argparse.ArgumentParser()
-group1 = parser.add_mutually_exclusive_group(required=True)
-group1.add_argument('--get', action='store', nargs='+', type=int)
-group1.add_argument('--set', action='store', nargs='+', type=int)
-group1.add_argument('--frequency', action='store', type=int, default=250)
-args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    group1 = parser.add_mutually_exclusive_group(required=True)
+    group1.add_argument('--get', action='store', nargs='+', type=int)
+    group1.add_argument('--set', action='store', nargs='+', type=int)
+    group1.add_argument('--frequency', action='store', type=int, default=250)
+    args = parser.parse_args()
 
-pca = PCA9685()
-pca.set_pwm_frequency(args.frequency)
-pca.output_enable()
+    pca = PCA9685()
+    pca.set_pwm_frequency(args.frequency)
+    pca.output_enable()
 
-if args.get:
-    for channel in args.get:
-        print(f'channel {channel} pwm: {pca.pwm[channel]}') 
-else:
-    for channel in args.set[:-1]:
-        pca.pwm[channel] = args.set[-1]
+    if args.get:
+        for channel in args.get:
+            print(f'channel {channel} pwm: {pca.pwm[channel]}') 
+    else:
+        for channel in args.set[:-1]:
+            pca.pwm[channel] = args.set[-1]
+
+if __name__ == '__main__':
+    main()
